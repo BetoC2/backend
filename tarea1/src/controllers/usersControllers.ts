8ef0eb2de6c91839e54a269e6a083105b2c5ef6a
Alberto <alberto.renteriac@iteso.mx>
2024-09-13 16:19:02 -0600

    texto corregido

--- tarea1/src/controllers/usersControllers.ts
+++ tarea1/src/controllers/usersControllers.ts
@@ -2,22 +2,7 @@ import { Request, Response } from 'express';
 
 class UsersController {
   getAll(req: Request, res: Response) {
-    res.send([{
-      id: 1,
-      name: "Alberto",
-      email: "alberto@email.com"
-    },
-    {
-      id: 2,
-      name: "Alberto2",
-      email: "alberto2@email.com"
-    },
-    {
-      id: 3,
-      name: "Alberto3",
-      email: "alberto3@email.com"
-    },
-    ])
+    res.send('lista de usuarios.')
   }
 }
 
